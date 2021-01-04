d10f9f860b72c7b1427141d9176a54a7fd7cef74
Andres Gamboa <andres24k8@gmail.com>
2021-01-04 12:52:48 -0600

    Updates

---
+++ precache-manifest.3be78d3cac767eee0db6bf5d250751a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284428e3a4b08d328becfa386edd85c9",
+    "url": "/countdownReactApp/index.html"
+  },
+  {
+    "revision": "8212e8ca166c9a855980",
+    "url": "/countdownReactApp/static/css/main.cc152724.chunk.css"
+  },
+  {
+    "revision": "3d05472c421b216ee162",
+    "url": "/countdownReactApp/static/js/2.e459ac7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countdownReactApp/static/js/2.e459ac7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8212e8ca166c9a855980",
+    "url": "/countdownReactApp/static/js/main.8f2ad51e.chunk.js"
+  },
+  {
+    "revision": "0aa6c8f24aa6acb3b30b",
+    "url": "/countdownReactApp/static/js/runtime-main.688f8770.js"
+  }
+]);
